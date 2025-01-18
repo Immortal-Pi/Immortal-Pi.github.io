124170395b78e5df58e59b5f82b2adb661c41593
amruth pai <26.amruth@gmail.com>
2025-01-18 14:13:46 -0600

    matrix

--- assets/js/matrix.js
+++ assets/js/matrix.js
@@ -7,10 +7,10 @@ canvas.height = window.innerHeight;
 const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
 const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const nums = '0123456789';
-const customWords = ['Immortal-Pi', 'Amruth', 'Pai', 'Texas','UT Dallas'];
+const customWords = ['Immortal-Pi', 'Amruth Pai', 'Pai', 'Texas','UT Dallas'];
 
 // const alphabet = katakana + latin + nums;
-const allCharacters = [...katakana, ...latin, ...nums, ...customWords];
+const allCharacters = [...katakana.repeat(3), ...latin.repeat(3), ...nums.repeat(3), ...customWords];
 const fontSize = 16;
 const columns = canvas.width/fontSize;
 const rainDrops = Array.from({ length: columns }).fill(canvas.height);
